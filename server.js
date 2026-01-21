58830ef9d2384da94ce5ec64377bffda3c0bb5a4
vogurt99 <vogurtsjrg@gmail.com>
2026-01-21 19:37:50 +0800

    Added middleware to server.js.

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./data/database');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(cors());
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', require('./routes/index'));
 
