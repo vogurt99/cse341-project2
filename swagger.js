0f7d1a4d31ed58452a0ec95385fe2bceacc8f690
vogurt99 <vogurtsjrg@gmail.com>
2026-01-21 20:10:42 +0800

    Added definitions to swagger.js.

--- swagger.js
+++ swagger.js
@@ -5,8 +5,23 @@ const doc = {
         title: 'Inventory Management API',
         description: 'API for managing products and categories',
     },
-    host: 'cse341-project-2-qp1l.onrender.com',
+    host: 'cse341-project-2-qp1l.onrender.com', 
     schemes: ['https'],
+    definitions: {
+        Product: {
+            productName: "any",
+            description: "any",
+            price: 0,
+            stockQuantity: 0,
+            category: "any",
+            manufacturer: "any",
+            restockDate: "any"
+        },
+        Category: {
+            categoryName: "any",
+            department: "any"
+        }
+    }
 };
 
 const outputFile = './swagger.json';
